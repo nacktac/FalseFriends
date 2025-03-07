077b2a7d83a5c44ff70dc8a9f51a8607c8abcb12
nacktac <165841910+nacktac@users.noreply.github.com>
2025-03-07 16:20:09 -0500

    Update gameboards.js

--- gameboards.js
+++ gameboards.js
@@ -28,81 +28,82 @@ const gameBoards = [
   },
   {
     id: 2,
-  rows: [
-    {
-      theme: "Famous Paintings",
-      clues: ["Nighthawks", "Mona Lisa", "Starry Night", "Scream"],
-      falseFriend: "Scream"
-    },
-    {
-      theme: "1 word for \"Leave!\"",
-      clues: ["Shoo", "Vamoose", "Scram", "Get Out"],
-      falseFriend: "Get Out"
-    },
-    {
-      theme: "Pronouns for People",
-      clues: ["He", "She", "They", "It"],
-      falseFriend: "It"
-    },
-    {
-      theme: "Things we've found in space",
-      clues: ["Meteor", "blackhole", "Satellite", "Alien"],
-      falseFriend: "Alien"
-    }
-  ],
-  falseFriendTheme: "Horror Movies"
+    rows: [
+      {
+        theme: "Famous Paintings",
+        clues: ["Nighthawks", "Mona Lisa", "Starry Night", "Scream"],
+        falseFriend: "Scream"
+      },
+      {
+        theme: "1 word for \"Leave!\"",
+        clues: ["Shoo", "Vamoose", "Scram", "Get Out"],
+        falseFriend: "Get Out"
+      },
+      {
+        theme: "Pronouns for People",
+        clues: ["He", "She", "They", "It"],
+        falseFriend: "It"
+      },
+      {
+        theme: "Things we've found in space",
+        clues: ["Meteor", "Blackhole", "Satellite", "Alien"],
+        falseFriend: "Alien"
+      }
+    ],
+    falseFriendTheme: "Horror Movies"
   },
   {
     id: 3,
- rows: [
-    {
-      theme: "Leaky Kitchen Gear",
-      clues: ["Funnel", "Colander", "Grater", "Bowls"],
-      falseFriend: "Bowls"
-    },
-    {
-      theme: "3D Arts",
-      clues: ["Ceramics", "Topiary", "Origami", "Painting"],
-      falseFriend: "Painting"
-    },
-    {
-      theme: "Regal Resting Places",
-      clues: ["Canopy Bed", "Liter", "Throne", "Statue"],
-      falseFriend: "Statue"
-    },
-    {
-      theme: "Inorganic Earthen Spoils",
-      clues: ["Minerals", "Rocks", "Gems", "Fossils"],
-      falseFriend: "Fossils"
-    }
-  ],
-  falseFriendTheme: "Museum Finds"
-},
+    rows: [
+      {
+        theme: "Leaky Kitchen Gear",
+        clues: ["Funnel", "Colander", "Grater", "Bowls"],
+        falseFriend: "Bowls"
+      },
+      {
+        theme: "3D Arts",
+        clues: ["Ceramics", "Topiary", "Origami", "Painting"],
+        falseFriend: "Painting"
+      },
+      {
+        theme: "Regal Resting Places",
+        clues: ["Canopy Bed", "Liter", "Throne", "Statue"],
+        falseFriend: "Statue"
+      },
+      {
+        theme: "Inorganic Earthen Spoils",
+        clues: ["Minerals", "Rocks", "Gems", "Fossils"],
+        falseFriend: "Fossils"
+      }
+    ],
+    falseFriendTheme: "Museum Finds"
+  },
   {
     id: 4,
-  rows: [
-    {
-      theme: "Sleep",
-      clues: ["Snooze", "Nap", "Slumber", "Alarm"],
-      falseFriend: "Alarm"
-    },
-    {
-      theme: "More than sufficient",
-      clues: ["Copious", "Abundant", "Ample", "Positive"],
-      falseFriend: "Positive"
-    },
-    {
-      theme: "Man Made Tools",
-      clues: ["Hammers", "Levers", "Doors", "Teeth"],
-      falseFriend: "Teeth"
-    },
-    {
-      theme: "Bright and Showy",
-      clues: ["Flamboyent", "Flashy", "Flare", "Flag"],
-      falseFriend: "Flag"
-    }
-  ],
-  falseFriendTheme: "False ____"
-}];
+    rows: [
+      {
+        theme: "Sleep",
+        clues: ["Snooze", "Nap", "Slumber", "Alarm"],
+        falseFriend: "Alarm"
+      },
+      {
+        theme: "More than sufficient",
+        clues: ["Copious", "Abundant", "Ample", "Positive"],
+        falseFriend: "Positive"
+      },
+      {
+        theme: "Man Made Tools",
+        clues: ["Hammers", "Levers", "Doors", "Teeth"],
+        falseFriend: "Teeth"
+      },
+      {
+        theme: "Bright and Showy",
+        clues: ["Flamboyant", "Flashy", "Flare", "Flag"],
+        falseFriend: "Flag"
+      }
+    ],
+    falseFriendTheme: "False ____"
+  }
+];
 
 window.gameBoards = gameBoards;
